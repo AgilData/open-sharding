7ed212f9fbe4f56e9c70b1d59e9353b382e049d4
Andy Grove <andy.grove@codefutures.com>
2011-10-20 15:59:21 -0600

    rename opensharding_mysql to myospfacade

--- libmyospfacade/scripts/build.sh
+++ libmyospfacade/scripts/build.sh
@@ -7,11 +7,11 @@ fi
 PLATFORM=$1
 LIB_DIR=$2
 
-make clean; make fifteen;       cp libopensharding_mysql* $LIB_DIR
-make clean; make fifteen_r;     cp libopensharding_mysql* $LIB_DIR
-#make clean; make fifteen_debug; cp libopensharding_mysql* $LIB_DIR/DEBUG/
+make clean; make fifteen;       cp libmyospfacade* $LIB_DIR
+make clean; make fifteen_r;     cp libmyospfacade* $LIB_DIR
+#make clean; make fifteen_debug; cp libmyospfacade* $LIB_DIR/DEBUG/
 
-make clean; make sixteen;       cp libopensharding_mysql* $LIB_DIR
-make clean; make sixteen_r;     cp libopensharding_mysql* $LIB_DIR
-#make clean; make sixteen_debug; cp libopensharding_mysql* $LIB_DIR/DEBUG/
+make clean; make sixteen;       cp libmyospfacade* $LIB_DIR
+make clean; make sixteen_r;     cp libmyospfacade* $LIB_DIR
+#make clean; make sixteen_debug; cp libmyospfacade* $LIB_DIR/DEBUG/
 
