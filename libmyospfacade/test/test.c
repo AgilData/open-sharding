8996a1e6668291b0a0dab0c819b6cda51f5c7cb1
Andy Grove <andy.grove@codefutures.com>
2011-10-25 16:03:16 -0600

    testing

--- libmyospfacade/test/test.c
+++ libmyospfacade/test/test.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <mysql.h>
+
+void run_query(MYSQL *my) {
+
+  const char *sql = "SELECT * FROM country";
+  
+  if (mysql_real_query(my, sql, strlen(sql))) {
+    printf("mysql_real_query() FAILED: %d %s\n", mysql_errno(my), mysql_error(my));
+    return;
+  }
+
+  printf("Query ran OK\n");
+}
 
 int main() {
   printf("TEST\n");
@@ -12,10 +25,14 @@ int main() {
     return -1;
   }
 
+  run_query(&my);
+
   if (mysql_select_db(&my, "dbs_junit_1b")) {
     printf("mysql_select_db() FAILED: %d %s\n", mysql_errno(&my), mysql_error(&my));
     return -1;
   }
+
+  run_query(&my);
 
   printf("TEST END\n");
 }
