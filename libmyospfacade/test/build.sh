42917b6a0b8f0df6c5020c086f208c48a7f4f5df
Andy Grove <andy.grove@codefutures.com>
2012-08-08 09:18:55 -0600

    build script

--- libmyospfacade/test/build.sh
+++ libmyospfacade/test/build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-gcc -I /usr/include/mysql multithreadtest.cpp -L /usr/lib64/myosp/ -lmysqlclient -ldl
+gcc -I /usr/include/mysql -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -fno-inline -MMD -MP multithreadtest.cpp
+gcc -o multithreadtest multithreadtest.o -lc -lstdc++ -ldl -lmysqlclient -llibboost_thread-gcc44-mt-1_38 -L/usr/lib64/myosp -L/usr/local/lib
+
+
