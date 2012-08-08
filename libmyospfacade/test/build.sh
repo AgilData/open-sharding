7a66a0a06d08ef2e203577f829ae018bf45ec2f4
Andy Grove <andy.grove@codefutures.com>
2012-08-08 09:11:05 -0600

    clean up test

--- libmyospfacade/test/build.sh
+++ libmyospfacade/test/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -I /usr/include/mysql multithreadtest.c -L /usr/lib64/myosp/ -lmysqlclient -ldl
+gcc -I /usr/include/mysql multithreadtest.cpp -L /usr/lib64/myosp/ -lmysqlclient -ldl
