d46f5cf43f86c61583b08211dea97d68abebcd05
Jerry Ramey <jerry.ramey@codefutures.com>
2012-08-10 17:13:02 -0600

    BUGS

--- libmyospfacade/src/mysql/MySQLDriver.cpp
+++ libmyospfacade/src/mysql/MySQLDriver.cpp
@@ -16,7 +16,7 @@
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 
 #include <unistd.h>
-#include <mysql_com.h>
+//#include <mysql_com.h>
 
 #include <stdio.h>
 #include <string.h>
