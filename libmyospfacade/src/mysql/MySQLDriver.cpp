1f26f276a9a08151fce05126ad48eed24c5c501b
Jerry Ramey <jerry.ramey@codefutures.com>
2012-08-10 15:33:28 -0600

    DEBUG

--- libmyospfacade/src/mysql/MySQLDriver.cpp
+++ libmyospfacade/src/mysql/MySQLDriver.cpp
@@ -15,6 +15,10 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 
+#include <unistd.h>
+#include <my_global.h>
+#include <mysql_com.h>
+
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
