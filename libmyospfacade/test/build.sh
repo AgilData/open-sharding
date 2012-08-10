cc6bdb88b8a7065f34e1ca6b0354d44df5ae09db
Andy Grove <andy.grove@codefutures.com>
2012-08-09 18:23:13 -0600

    debug

--- libmyospfacade/test/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-gcc -I /usr/include/mysql -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -fno-inline -MMD -MP multithreadtest.cpp
-gcc -o multithreadtest multithreadtest.o -lc -lstdc++ -ldl -lmysqlclient -L/usr/lib64/myosp -L/usr/local/lib
-
-
