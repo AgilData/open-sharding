e41efd8a1d7aae8638ae8dcac6d3b0e62a7c5ca3
Jerry Ramey <jerry.ramey@codefutures.com>
2012-08-13 16:13:27 -0600

    Directory change fix

--- build.rb
+++ build.rb
@@ -85,7 +85,7 @@ def mysql_install(mysql_version)
         puts "Invalid version for installation of mysql."
         exit
     end
-    Dir.chdir(mysql_dir)
+   run_command("cd #{mysql_dir}")
     run_command("./configure --prefix=/usr/local/mysql")
     run_command("make")
     run_command("make install")
