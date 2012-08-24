ca231b8b3027141fb6e366781c6575750e54bd6b
Jerry Ramey <jerry.ramey@codefutures.com>
2012-08-23 18:26:56 -0600

    Fixed directory change for mysql 5.1

--- build.rb
+++ build.rb
@@ -116,6 +116,7 @@ def mysql_install(mysql_version)
      elsif mysql_version.match("5.1")
        run_command("svn export https://subversion.assembla.com/svn/open-sharding-test/trunk/mysql/mysql-5.1.65-myosp.tar.gz/")
        run_command("tar xvfz mysql-5.1.65-myosp.tar.gz -C #{mysql_dir}")
+         mysql_dir = "./mysql-install/#{mysql_version}/mysql-5.1.65"
        Dir.chdir("#{mysql_dir}")
     else
        puts "Invalid version for installation of mysql."
