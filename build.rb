617633b111cbdbb051ba369ab9d0165d6ff83303
Jerry Ramey <jerry.ramey@codefutures.com>
2012-08-25 14:32:09 -0600

    Using File Utils for Copying

--- build.rb
+++ build.rb
@@ -140,12 +140,15 @@ def mysql_install(mysql_version)
     run_command("bin/mysql_install_db --user=mysql")
     run_command("chown -R root .")
     #run_command("chown -R mysql var")
-    run_command("cp #{mysql_dir}/support-files/my-medium.cnf /etc/my.cnf")
+    FileUtils.cp "#{mysql_dir}/support-files/my-medium.cnf /etc/my.cnf"
+    #run_command("cp #{mysql_dir}/support-files/my-medium.cnf /etc/my.cnf")
     run_command("bin/mysqld_safe --user=mysql &")
-    run_command("cp #{mysql_dir}/support-files/mysql.server /etc/init.d/mysql.server")
+    FileUtils.cp "#{mysql_dir}/support-files/mysql.server /etc/init.d/mysql.server"
+    #run_command("cp #{mysql_dir}/support-files/mysql.server /etc/init.d/mysql.server")
     run_command("ln bin/mysql /usr/bin/mysql")
     Dir.mkdir("/usr/local/include/mysql")
-    run_command("cp -r #{mysql_dir}/include/* /usr/local/include/mysql/")
+    FileUtils.cp "#{mysql_dir}/include/* /usr/local/include/mysql/"
+    #run_command("cp -r #{mysql_dir}/include/* /usr/local/include/mysql/")
 end
     
     
