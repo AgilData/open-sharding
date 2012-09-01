54933ce0e4b5387f53d4d301c7f690692707256a
Jerry Ramey <jerry.ramey@codefutures.com>
2012-08-31 18:40:32 -0600

    More changes

--- build.rb
+++ build.rb
@@ -16,20 +16,6 @@ require 'fileutils'
 ## Constants
 #################################################################################################
 
-BRANCH = "master"
-REPOSITORY = "git://git.assembla.com/open-sharding.git"
-BRANCH_LIST = ["HEAD", "logging_changes", "osp-virtual-hosts","master", "myospbinlog", "mysql_compatibility", "osp-client-host-list", "threaded_fifo", "windows"]
-BOOST_VER = "1.38.0"
-BOOST_DIR = "boost_1_38_0"
-BOOST_TAR = "boost_1_38_0.tar.gz"
-BOOST_DOWNLOAD = "http://downloads.sourceforge.net/project/boost/boost/#{BOOST_VER}/#{BOOST_TAR}"
-INIT_DIR = Dir.pwd
-OS_DIR = "#{INIT_DIR}/open-sharding"
-OPERSYS = "ubuntu1104" # update this to set the build operating system
-ARCH = "x64" # update this to set the build architecture
-LIBMYOSP_VER = "pre-release-1.1.113" # update this to set the libmyosp version
-LIBMYOSP_TAR = "libmyosp-#{OPERSYS}-#{ARCH}-#{LIBMYOSP_VER}.tar.gz"
-
 #################################################################################################
 ## Run an operating system command
 #################################################################################################
@@ -40,61 +26,107 @@ def run_command(cmd)
         exit 1
     end
 end
-
 #################################################################################################
-## Pull Opensharding Branch
+## Get Platform
 #################################################################################################
-def clone_rep(branch_name)
-  # This method depends on having the repo checked out already in the current folder.
-    puts "Cloning Opensharding Repositories"
-    if !File.exists? "open-sharding"
-        puts "WARN: open-sharding Not Found, aborting."
-        return
+def get_platform
+    if RUBY_PLATFORM.downcase =~ /darwin/
+        return "mac"
+        elsif RUBY_PLATFORM.downcase =~ /windows/
+        return "win"
+        else
+        
+        # get linux distro name and version (might now work on all distros)
+        issue = `cat /etc/issue`.downcase.split
+        arch = `uname -i`
+        platform = "#{issue} #{arch}"
+        return platform
     end
-    cmd = "git pull origin #{branch_name}"
-    run_command cmd
 end
-
+#################################################################################################
+## Install Dependencies
+#################################################################################################
+def install_dependencies
+    puts "Installing Dependencies *****************************************"
+    platform = get_platform
+    if platform.match("centos")
+        run_command("yum install -y gcc e2fsprogs-devel ncurses-devel libtool-ltdl-devel python-devel subversion-devel openssl-devel java-1.6.0-openjdk-devel kernel-devel uuid-devel")
+        elsif platform.match("ubuntu") || platform.match("debian")
+        run_command("sudo apt-get install -y build-essential libncurses5-dev libltdl3-dev uuid-dev")
+        else
+        puts "Platform not found: #{platform}"
+        exit
+    end
+end
 #################################################################################################
 ## Building libmyosp
 #################################################################################################
-def build(branch_name)
-    check_dep
-    puts "Building from branch #{branch_name}"
-    clone_rep branch_name
-    puts "Downloading Boost Libraries"
-    if RUBY_PLATFORM.downcase.include?("darwin")
-        if !File.exists? "#{BOOST_DIR}"
-            run_command "curl -L #{BOOST_DOWNLOAD} | tar xz"
-        end
-    else
-        if !File.exists? "#{BOOST_TAR}"
-            run_command "wget #{BOOST_DOWNLOAD}"
-        end
-        if !File.exists? "#{BOOST_DIR}"
-            run_command "tar xzf #{BOOST_TAR}"
-        end
+def build(mysql_version)
+    
+    puts "Building MyOSP *****************************************"
+    
+    # we need to make sure we already have the mysql headers for the _real libs since we need
+    # to compile against the same exact version or we will end up with hard to diagnose segmentation
+    # faults at runtime
+    if mysql_version.match("5.0")
+        mysql_real_dir = "mysql-install-#{mysql_version}/mysql-5.0.45"
+        elsif mysql_version.match("5.1")
+        mysql_real_dir = "mysql-install-#{mysql_version}/mysql-5.1.36"
+        else
+        puts "Support for mysql #{mysql_version} is not avaliable."
+        exit -1
     end
-    puts "Building and Installing Boost Libraries"
-    Dir.chdir BOOST_DIR
-    run_command "./configure"
-    run_command "sudo make install"
-    Dir.chdir OS_DIR
-    puts "Building and installing libopensharding"
-    Dir.chdir "libopensharding"
-    run_command "make clean"
-    run_command "make"
-    run_command "sudo make install"
-    Dir.chdir OS_DIR
+    
+    if !File.exists?(mysql_real_dir)
+        puts "Could not locate mysql directory '#{mysql_real_dir}' - you should run build-real first (this is a one-time requirement)"
+        exit -1
+    end
+    
+    # check for any libopensharding.so already deployed that might be on LD_LIBRARY_PATH
+    # this is REALLY important or we will end up with corrupt binaries or a mix
+    # of old and new functionality
+    
+    # look for libopensharding
+    puts "Searching for deployed versions of libopensharding..."
+    deployed_opensharding_libs = `find /usr -name \"libopensharding*\"`
+    #puts "FOUND: #{deployed_opensharding_libs}"
+    if deployed_opensharding_libs != ""
+        puts "Cannot build until you delete these already deployed versions: #{deployed_opensharding_libs}"
+        exit
+    end
+    puts "No deployed versions of libopensharding found"
+    
+    # build libopensharding
+    puts "Building libopensharding"
+    run_command "cd libopensharding ; ruby build.rb"
+    
+    # build libmyosp
     puts "Building libmyosp"
-    Dir.chdir "libmyospfacade"
-    run_command "ant"
-    puts "Creating libmyosp tarball"
-    run_command "./scripts/mktarball.sh #{LIBMYOSP_TAR} libs"
-    puts "Placing tarball in #{INIT_DIR}"
-    run_command "mv #{LIBMYOSP_TAR} #{INIT_DIR}"
-    Dir.chdir INIT_DIR
+    run_command "cd libmyospfacade ; ruby build.rb release #{mysql_version}"
+    
 end
+
+#################################################################################################
+## Building MySQl
+#################################################################################################
+def boost-build
+    puts "Down loading the boost libraries."
+    cmd = 'wget http://downloads.sourceforge.net/project/boost/boost/1.38.0/boost_1_38_0.tar.gz'
+    run_command cmd
+    cmd = 'tar xvfz boost_1_38_0.tar.gz'
+    run_command cmd
+    Dir.chdir "boost_1_38_0"
+    puts "Changed Directory to boost_1_38_0"
+    cmd = './configure --with-libraries=thread'
+    run_command cmd
+    cmd = 'make'
+    run_command cmd
+    cmd = 'sudo make install'
+    run_command cmd
+    
+end
+
+
 
 #################################################################################################
 ## Check Dependencies
@@ -147,26 +179,6 @@ def check_dep
     end
     puts "Found"
     
-    print "Ant ... "
-    x = `which ant`.strip
-    if x == ""
-        puts ""
-        puts "Error: Ant is missing"
-        exit 1
-    end
-    puts "Found"
-    
-    print "UUID ... "
-    x = `find /usr/local/include -name 'uuid.h'`.strip
-    if x == ""
-        x = `find /usr/include -name 'uuid.h'`.strip
-        if x == ""
-            puts ""
-            puts "Error: uuid/uuid.h is missing"
-            exit 1
-        end
-    end
-    puts "Found"
     
     puts "All Dependencies are found"
 end
@@ -176,36 +188,53 @@ end
 #################################################################################################
 begin
     if ARGV.length==0
-        puts "Usage: ruby build.rb OPTION [ARGS]"
+        puts "Usage: ruby build.rb OPTION [ARGS] [MYSQL VERSION]"
         puts "\tAvailable options: "
-        puts "\t\t - build [opensharding-branch]"
-        exit 
+        puts "\t\t - setup-env "
+        puts "\t\t - build (Installation of myosp only)"
+        puts "\t\t - build-boost (Installation of boost and dependencies. Only needs to be executed once.)"
+        puts "\t MYSQL VERSION OPTIONS"
+        puts "\t\t - 5.0 "
+        puts "\t\t - 5.1 "
+        exit
     end
     
     option = ARGV[0]
     
-    username = `whoami`.strip
-    if username != 'root'
-        puts "This script must be run as the root user (not as '#{username}')"
-        exit 1
-    end
-    
     if option == "build"
-        if ARGV.length < 2
-            puts "Warn: No branch specified to build, defaulting to #{BRANCH}"
-            build BRANCH
-        else
-            proposed_branch = ARGV[1]
-            if !BRANCH_LIST.include? proposed_branch
-                puts "Error: #{proposed_branch} is not a valid branch"
-                exit 1
-            else
-                build proposed_branch
-            end
+        puts "Building with the option: #{option}"
+        if ARGV.length==1
+            puts "Usage: ruby build.rb build [MYSQL VERSION]"
+            puts "\tAvailable options: "
+            puts "\t\t - 5.0 "
+            puts "\t\t - 5.1 "
+            exit
         end
-    elsif option == "check_dep"
+        mysql_version = ARGV[1]
+        if mysql_version.match("5.0") || mysql_version.match("5.1")
+            build(mysql_version)
+            else
+            puts "Error: Not supported MySQL type."
+        end
+        elsif option == "build-boost"
+        puts "Building with the option: #{option}"
+        if ARGV.length==1
+            puts "Usage: ruby build.rb build-real [MYSQL VERSION]"
+            puts "\tAvailable options: "
+            puts "\t\t - 5.0 "
+            puts "\t\t - 5.1 "
+            exit
+        end
+        mysql_version = ARGV[1]
+        if ! mysql_version.match("5.0") &&  ! mysql_version.match("5.1")
+            puts "Error: Not supported MySQL type."
+            exit
+        end
+        install_dependencies
+        boost-build
+        elsif option == "check-dep"
         check_dep
-    else
+        else
         puts "Error: Invalid Argument"
         exit 1
     end
