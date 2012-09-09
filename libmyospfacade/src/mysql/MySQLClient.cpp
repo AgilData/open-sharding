c130254f72519b2b9cf098143985f87807872d0f
Jerry Ramey <jerry.ramey@codefutures.com>
2012-09-09 16:02:29 -0600

    debug

--- libmyospfacade/src/mysql/MySQLClient.cpp
+++ libmyospfacade/src/mysql/MySQLClient.cpp
@@ -844,11 +844,8 @@ int MySQLClient::mysql_drop_db(MYSQL *mysql, const char *DB){
 
 void MySQLClient::my_net_local_init(NET *net)
     {
-        net->max_packet=   (uint) net_buffer_length;
-        my_net_set_read_timeout(net, CLIENT_NET_READ_TIMEOUT);
-        my_net_set_write_timeout(net, CLIENT_NET_WRITE_TIMEOUT);
-        net->retry_count=  1;
-        net->max_packet_size= max(net_buffer_length, max_allowed_packet);
+        my_net_local_initFnType* tempFunction = (my_net_local_initFnType*)get_mysql_function("my_net_local_init");
+        tempFunction(stmt, offset);
     }
 
 
