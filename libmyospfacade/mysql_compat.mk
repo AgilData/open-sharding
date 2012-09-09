e33e47cdc2d40453dbac7288e16ac8acd923402c
Jerry Ramey <jerry.ramey@codefutures.com>
2012-09-09 11:28:49 -0600

    Fixing paths for make files.

--- libmyospfacade/mysql_compat.mk
+++ libmyospfacade/mysql_compat.mk
@@ -4,8 +4,8 @@
 # This is temporary, we need to settle how we are going to do the final build across all platforms
 # Currently, you must have the mysql 5.0 and 5.1 source installed and compiled to build myosp
 
-MYSQL50_SRC_PATH=/root/mysql-install-5.0/mysql5.0.96/
-MYSQL51_SRC_PATH=/root/mysql-install-5.1/mysql5.1.65/
+MYSQL50_SRC_PATH=/root/open-sharding/mysql-install-5.0/mysql-5.0.96/
+MYSQL51_SRC_PATH=/root/open-sharding/mysql-install-5.1/mysql-5.1.65/
 
 ###############
 #  MYSQL 5.0  #
