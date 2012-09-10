f21706148a972b7d2304127b5a5449060ce8a1e2
Jerry Ramey <jerry.ramey@codefutures.com>
2012-09-10 09:57:19 -0600

    Fixing compatibility

--- libmyospfacade/test/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-gcc -I /usr/include/mysql -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -fno-inline -MMD -MP multithreadtest.cpp
-gcc -o multithreadtest multithreadtest.o -lc -lstdc++ -ldl -lmysqlclient -lboost_thread-gcc44-mt-1_38 -L/usr/lib64/myosp -L/usr/local/lib
-
-
