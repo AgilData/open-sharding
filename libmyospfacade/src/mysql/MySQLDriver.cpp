e867e0aad93eb48ee76959cb1913e700519afe34
Jerry Ramey <jerry.ramey@codefutures.com>
2012-09-12 20:50:39 -0600

    Adding functions

--- libmyospfacade/src/mysql/MySQLDriver.cpp
+++ libmyospfacade/src/mysql/MySQLDriver.cpp
@@ -167,7 +167,8 @@ const char *client_errors[]=
   "Lost connection to MySQL server at '%s', system error: %d",
   "Statement closed indirectly because of a preceeding %s() call",
   "The number of columns in the result set differs from the number of bound buffers. You must reset the statement, rebind the result set columns, and execute the statement again",
-  ""
+  "",
+  NullS
 };
 
 //QUESTIONABLE ISSUES//
