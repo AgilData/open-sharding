1f376eaade99fa7db466d8cfb8081f2fe5fbc01d
Jerry Ramey <jerry.ramey@codefutures.com>
2012-09-13 12:50:24 -0600

    Adding new functions to handle termination.

--- libmyospfacade/src/mysql/MySQLDriver.cpp
+++ libmyospfacade/src/mysql/MySQLDriver.cpp
@@ -14,17 +14,18 @@
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
-#include "my_global.h"
+
+// if we want to support all mysql symbols we need to *at least* import these headers
+#include <my_global.h>
+#include <m_ctype.h>
+
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
 #include <stdlib.h>
 #include <map>
 
-// if we want to support all mysql symbols we need to *at least* import these headers
-//include "m_ctype.h"
 
-//#include <my_sys.h>
 #include <mysql.h>
 #include <errmsg.h>
 
