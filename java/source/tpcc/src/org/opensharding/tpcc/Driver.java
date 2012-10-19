d9c9ec52d850417bc20b0244f25d4876cab226f9
Jerry Randell Ramey <jramey@bb136-06.mines.edu>
2012-10-19 13:58:56 -0600

    Debug

--- java/source/tpcc/src/org/opensharding/tpcc/Driver.java
+++ java/source/tpcc/src/org/opensharding/tpcc/Driver.java
@@ -88,10 +88,10 @@ public class Driver implements TpccConstants {
 	      case 0:
 			doNeword(t_num, conn, pStmts);
 			break;
-//		  case 1:
-//			  System.out.println("Executing do payment");
-//			doPayment(t_num, conn, pStmts);
-//			break;
+		  case 1:
+			  System.out.println("Executing do payment");
+			doPayment(t_num, conn, pStmts);
+			break;
 //		  case 2:
 //			  System.out.println("Executing do ord stat");
 //			doOrdstat(t_num, conn, pStmts);
