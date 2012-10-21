d6fd68d8764ff3d5f09f805ad3b84100998a8431
Jerry Ramey <jerry.ramey@codefutures.com>
2012-10-21 17:23:07 -0600

    Load fix

--- java/source/tpcc/src/org/opensharding/tpcc/TpccLoad.java
+++ java/source/tpcc/src/org/opensharding/tpcc/TpccLoad.java
@@ -198,9 +198,9 @@ public class TpccLoad implements TpccConstants {
 		if(particle_flg==0){
 			System.out.printf("Particle flag: %d", particle_flg);
 			//Load.loadItems(conn, shardCount, option_debug);
-		    Load.loadWare(conn, shardCount, (int)min_ware, (int)max_ware, option_debug);
-		   // Load.loadCust(conn, shardCount, (int)min_ware, (int)max_ware);
-		 //   Load.loadOrd(conn, shardCount, (int)min_ware, (int)max_ware);
+		   // Load.loadWare(conn, shardCount, (int)min_ware, (int)max_ware, option_debug);
+		   Load.loadCust(conn, shardCount, (int)min_ware, (int)max_ware);
+		   Load.loadOrd(conn, shardCount, (int)min_ware, (int)max_ware);
 		}else if(particle_flg==1){
 		    switch(part_no){
 			case 1:
