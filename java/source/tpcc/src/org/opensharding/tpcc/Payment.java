5d417f3e2f7545813e04c9aec09c1b8d234f1473
Jerry Ramey <jerry.ramey@codefutures.com>
2012-11-01 13:24:14 -0600

    Adding driver changes

--- java/source/tpcc/src/org/opensharding/tpcc/Payment.java
+++ java/source/tpcc/src/org/opensharding/tpcc/Payment.java
@@ -92,19 +92,19 @@ public class Payment implements TpccConstants{
 	
 			proceed = 1;
 			
-			//Get prepared statement
-			//"UPDATE warehouse SET w_ytd = w_ytd + ? WHERE w_id = ?"
-			try {
-				
-				pStmts.getStatement(9).setFloat(1, h_amount);
-				pStmts.getStatement(9).setInt(2, w_id);
-				if(TRACE) logger.trace("UPDATE warehouse SET w_ytd = w_ytd + " + h_amount + " WHERE w_id = "+ w_id);
-				pStmts.getStatement(9).executeUpdate();
-				count.increment();
-
-			} catch (SQLException e) {
-				throw new Exception("Payment Update transaction error", e);
-			}
+//			//Get prepared statement
+//			//"UPDATE warehouse SET w_ytd = w_ytd + ? WHERE w_id = ?"
+//			try {
+//				
+//				pStmts.getStatement(9).setFloat(1, h_amount);
+//				pStmts.getStatement(9).setInt(2, w_id);
+//				if(TRACE) logger.trace("UPDATE warehouse SET w_ytd = w_ytd + " + h_amount + " WHERE w_id = "+ w_id);
+//				pStmts.getStatement(9).executeUpdate();
+//				count.increment();
+//
+//			} catch (SQLException e) {
+//				throw new Exception("Payment Update transaction error", e);
+//			}
 			
 			proceed = 2;
 			//Get prepared statement
